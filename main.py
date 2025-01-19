841de6c1faadf878e1a342702de8638b1d340474
Jason <139952340+PensiveSpy@users.noreply.github.com>
2025-01-19 13:32:10 -0700

    Edited the main file to get news article

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import time
 import requests
 import webbrowser
 from DailyNews.hello import my_api_key
+from DailyNews.hello import receiver_email
+from DailyNews.hello import sender_email
 
 country = 'ca'
-url = f"https://newsapi.org/v2/top-headlines?country={country}&category=general&apiKey={my_api_key}"
+URL = f"https://newsapi.org/v2/top-headlines?country={country}&category=general&apiKey={my_api_key}"
 
 def get_news():
-
-def get_email():
-
+    news = requests.get(URL)
+    if news.status_code == 200:
+        news_data = news.json()
+        articles = news_data.get('articles', [])
+        news_list = []
+        for article in articles:
+            title = article.get('title')
+            description = article.get('description')
+            url = article.get('url')
+            news_list.append(f"Title: {title}\nDescription: {description}\nURL: {url}\n\n")
+        return ''.join(news_list)
+    else:
+        return 'Failed to fetch news.'
 
