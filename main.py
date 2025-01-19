52313f47e7eeeed3567db237a137e63b7944f649
Jason <139952340+PensiveSpy@users.noreply.github.com>
2025-01-19 13:11:34 -0700

    Fixed the URL along with creating functions to get and send news

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import time
+import requests
+import webbrowser
 from DailyNews.hello import my_api_key
 
 country = 'ca'
+url = f"https://newsapi.org/v2/top-headlines?country={country}&category=general&apiKey={my_api_key}"
 
-url = f'https://newsapi.org/v2/top-headlines?country={country}&pageSize=20&apiKey={my_api_key}'
+def get_news():
+
+def get_email():
+
+
