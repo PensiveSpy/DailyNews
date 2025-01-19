43997e2c1978867e88cfa82b6ab60741dfb1c9f4
Jason <139952340+PensiveSpy@users.noreply.github.com>
2025-01-19 12:43:21 -0700

    Added the url to enable communication

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-import requests
+import time
+from DailyNews.hello import my_api_key
+
+country = 'ca'
+
+url = f'https://newsapi.org/v2/top-headlines?country={country}&pageSize=20&apiKey={my_api_key}'
