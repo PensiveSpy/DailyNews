d1adb100dda7e1fcd90413a524c246d9dba98874
30208299 <jason.bakajika@ucalgary.ca>
2025-01-26 13:08:51 -0700

    Edited the source code along with adding additional details on hidden file.

--- main.py
+++ main.py
@@ -8,11 +8,11 @@ from DailyNews.hello import sender_email
 from DailyNews.hello import my_password
 
 country = 'ca'
-URL = f"https://newsapi.org/v2/top-headlines?country={country}&category=general&apiKey={my_api_key}"
 
 
 def get_news():
-    news = requests.get(URL)
+    url = f"https://newsapi.org/v2/top-headlines?country={country}&category=general&apiKey={my_api_key}"
+    news = requests.get(url)
     if news.status_code == 200:
         news_data = news.json()
         articles = news_data.get('articles', [])[:20]
@@ -45,4 +45,19 @@ def send_email(news_string, sender_email, receiver_email):
         server.quit()
 
 
+def main():
+    news = get_news()
+    if news:
+        send_email(news, sender_email, receiver_email)
+    else:
+        print('Failed to get the news')
 
+
+schedule.every().day.at('08:00').do(main)
+
+if __name__ == "__main__":
+    main()
+
+    while True:
+        schedule.run_pending()
+        time.sleep(60)
